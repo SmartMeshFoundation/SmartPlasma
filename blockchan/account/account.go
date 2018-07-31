90372dd5ececea32240b7c715ec7b4c45198687e
IntegralTeam <repo@sfxdx.ru>
2018-07-31 12:38:52 +0700

    Added PlasmaKeyedTransactor.

--- blockchan/account/account.go
+++ blockchan/account/account.go
@@ -7,7 +7,22 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/pkg/errors"
+
+	"github.com/smartmeshfoundation/smartplasma/blockchan/transaction"
 )
+
+// PlasmaSignerFn is a signer function callback when requires a method
+// to sign the Plasma cash transaction before submission.
+type PlasmaSignerFn func(address common.Address,
+	tx *transaction.Transaction) (*transaction.Transaction, error)
+
+// PlasmaTransactOpts is the collection of authorization data required
+// to create a valid Plasma Cash transaction.
+type PlasmaTransactOpts struct {
+	PlasmaSigner PlasmaSignerFn
+	*bind.TransactOpts
+}
 
 // GenKey makes new private key.
 func GenKey() *ecdsa.PrivateKey {
@@ -20,12 +35,12 @@ func GenKey() *ecdsa.PrivateKey {
 }
 
 // Account makes new account.
-func Account(key *ecdsa.PrivateKey) *bind.TransactOpts {
-	return bind.NewKeyedTransactor(key)
+func Account(key *ecdsa.PrivateKey) *PlasmaTransactOpts {
+	return NewPlasmaKeyedTransactor(key)
 }
 
 // GenAccounts makes new accounts.
-func GenAccounts(number int) (accounts []*bind.TransactOpts) {
+func GenAccounts(number int) (accounts []*PlasmaTransactOpts) {
 	for i := 0; i < number; i++ {
 		accounts = append(accounts, Account(GenKey()))
 	}
@@ -33,14 +48,31 @@ func GenAccounts(number int) (accounts []*bind.TransactOpts) {
 }
 
 // Address makes new public key.
-func Address(acc *bind.TransactOpts) common.Address {
+func Address(acc *PlasmaTransactOpts) common.Address {
 	return acc.From
 }
 
 // Addresses extracts public keys from accounts.
-func Addresses(accounts []*bind.TransactOpts) (addresses []common.Address) {
+func Addresses(accounts []*PlasmaTransactOpts) (addresses []common.Address) {
 	for _, a := range accounts {
 		addresses = append(addresses, Address(a))
 	}
 	return
+}
+
+// NewPlasmaKeyedTransactor is a utility method to easily create
+// a transaction signer from a single private key.
+func NewPlasmaKeyedTransactor(key *ecdsa.PrivateKey) *PlasmaTransactOpts {
+	keyAddr := crypto.PubkeyToAddress(key.PublicKey)
+
+	return &PlasmaTransactOpts{
+		TransactOpts: bind.NewKeyedTransactor(key),
+		PlasmaSigner: func(address common.Address,
+			tx *transaction.Transaction) (*transaction.Transaction, error) {
+			if address != keyAddr {
+				return nil, errors.New("not authorized to sign this account")
+			}
+			return tx.SignTx(key)
+		},
+	}
 }
