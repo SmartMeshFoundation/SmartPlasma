fa99231c308d307f8614af2ac0b6ab37720ad630
IntegralTeam <repo@sfxdx.ru>
2018-08-28 17:15:15 +0700

    Added Mediator comments.

--- solidity/Mediator.sol
+++ solidity/Mediator.sol
@@ -37,17 +37,27 @@ contract RootChain {
 }
 
 /**
- * @title Mediator
+ * @title Mediator contract for SmartPlasma.
+ *
+ * Accumulates all deposits. Makes transfer tokens.
  */
 contract Mediator is Ownable {
     RootChain public rootChain;
 
+    /** @dev If it is true, than RootChain contact already joined. */
     bool joined;
 
+    /** @dev Dictionary with deposits.
+     *
+     *  key - unique identifier of a deposit (uid).
+     *  value - information about a deposit.
+     */
     mapping(bytes32 => entry) cash;
 
     struct entry {
+        /// Currency address.
         address currency;
+        /// Amount amount of currency.
         uint amount;
     }
 
@@ -55,6 +65,9 @@ contract Mediator is Ownable {
         joined = false;
     }
 
+    /** @dev Joins new RootChain contact to Mediator contract.
+     *  @param plasma Address of RootChain contact.
+     */
     function joinPlasma(address plasma) public onlyOwner {
         require(!joined);
 
@@ -62,31 +75,50 @@ contract Mediator is Ownable {
         joined = true;
     }
 
+    /** @dev Checks token before deposit to Smart Plasma.
+     *
+     *  The user can call this function on his own to check the possibility
+     *  of making a deposit to the Smart Plasma contract.
+     *  This helper function can serve to verify a token contract in the future.
+     *  @param addr Address of token contract.
+     */
     function checkToken(address addr) view public returns(bool) {
         Token token = Token(addr);
 
-        require(token.totalSupply() > 0); // checkToken test1
-        require(token.balanceOf(msg.sender) > 0); // checkToken test2
-        require(token.approve(this, 0)); // checkToken test3
-        require(token.increaseApproval(this, 0)); // checkToken test4
-        require(token.transferFrom(msg.sender, this, 0)); // checkToken test5
+        require(token.totalSupply() > 0); /// checkToken test1
+        require(token.balanceOf(msg.sender) > 0); /// checkToken test2
+        require(token.approve(this, 0)); /// checkToken test3
+        require(token.increaseApproval(this, 0)); /// checkToken test4
+        require(token.transferFrom(msg.sender, this, 0)); /// checkToken test5
 
         return true;
     }
 
+    /** @dev Adds deposits on Smart Plasma.
+     *  @param currency Currency address.
+     *  @param amount Amount amount of currency.
+     */
     function deposit(address currency, uint amount) public {
         require(amount > 0);
 
         Token token = Token(currency);
-        token.transferFrom(msg.sender, this, amount); // deposit test1
+        token.transferFrom(msg.sender, this, amount); /// deposit test1
 
-        bytes32 uid = rootChain.deposit(msg.sender, currency, amount); // deposit test2
+        bytes32 uid = rootChain.deposit(msg.sender, currency, amount); /// deposit test2
         cash[uid] = entry({
             currency: currency,
             amount: amount
         });
     }
 
+    /** @dev withdraws deposit from Smart Plasma.
+     *  @param prevTx Penultimate deposit transaction.
+     *  @param prevTxProof Proof of inclusion of a penultimate transaction in a Smart Plasma block.
+     *  @param prevTxBlkNum The number of the block in which the penultimate transaction is included.
+     *  @param txRaw lastTx Last deposit transaction.
+     *  @param txProof Proof of inclusion of a last transaction in a Smart Plasma block.
+     *  @param txBlkNum The number of the block in which the last transaction is included.
+     */
     function withdraw(
         bytes prevTx,
         bytes prevTxProof,
@@ -110,8 +142,8 @@ contract Mediator is Ownable {
         entry invoice = cash[uid];
 
         Token token = Token(invoice.currency);
-        token.transfer(msg.sender, invoice.amount); // withdraw test 1
+        token.transfer(msg.sender, invoice.amount); /// withdraw test 1
 
-        delete(cash[uid]); // withdraw test 2
+        delete(cash[uid]); /// withdraw test 2
     }
 }
